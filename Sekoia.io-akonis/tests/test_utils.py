d1cad364cb48af1375322ed6328a6b6b3607d406
Imothep-Akonis <cibrahim@akonis.fr>
2025-12-17 10:54:38 +0100

    Removed unused Sekoia.io-a* dir

--- Sekoia.io-akonis/tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-from sekoiaio.utils import user_agent
-
-
-def test_user_agent():
-    user_agent_orig = user_agent()
-
-    agent, version = user_agent_orig.split("/", 1)
-    assert agent == "symphony-module-sekoia.io"
-    assert version != "unknown"
